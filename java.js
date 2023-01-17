24d2139ea9181350028892ca9da00ec81a4de6e0
santiago52313 <122923346+santiago52313@users.noreply.github.com>
2023-01-17 21:52:54 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function convert() {
+  const amount = document.getElementById("amount").value;
+  const fromCurrency = document.getElementById("from-currency").value;
+  const toCurrency = document.getElementById("to-currency").value;
+  let result;
+  
+  // Code to fetch exchange rate and convert the amount here
+  // For example, using an API like Open Exchange Rates
+  // let exchangeRate = getExchangeRate(fromCurrency, toCurrency);
+ 
